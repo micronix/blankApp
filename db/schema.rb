89275cb51a38e60c15d11009bc0ea2249e44c643
Ruben <ssrcruz@gmail.com>
2015-06-24 18:28:01 -0400

    Add Devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150620163733) do
+ActiveRecord::Schema.define(version: 20150624200933) do
+
+  create_table "api_v1_comments", force: :cascade do |t|
+    t.string   "user"
+    t.integer  "site_id"
+    t.string   "type"
+    t.string   "memo"
+    t.string   "alternatives"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "api_v1_comments", ["site_id"], name: "index_api_v1_comments_on_site_id"
+
+  create_table "api_v1_sites", force: :cascade do |t|
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "hosts", force: :cascade do |t|
     t.string "url",    null: false
@@ -33,5 +51,23 @@ ActiveRecord::Schema.define(version: 20150620163733) do
   end
 
   add_index "paths", ["host_id"], name: "index_paths_on_host_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
